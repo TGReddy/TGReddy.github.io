27a6c2b7576468d08bc9ae1bb80234dde0e136f0
TGReddy <thomasgeorgereddy@gmail.com>
2020-12-21 00:22:01 +0000

    h III

---
+++ _site/js/sidebar.js
@@ -0,0 +1,18 @@
+function loopDropdowns() {
+  divs = document.getElementsByClassName('sidebarDropdown');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].style.height = sessionStorage.getItem(divs[i].id);
+  }
+}
+
+function toggleDropdown(id) {
+  var growDiv = document.getElementById(id);
+  if (sessionStorage.getItem(id) == '0') {
+    sessionStorage.setItem(id, document.getElementById(id + "Wrap").clientHeight + "px");
+    growDiv.style.height = sessionStorage.getItem(id);
+  } else {
+    sessionStorage.setItem(id, '0');
+    growDiv.style.height = sessionStorage.getItem(id);
+  }
+  loopDropdowns();
+}
