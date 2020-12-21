aebb97a64ea161d88e1e588b70f6102a5454589f
TGReddy <thomasgeorgereddy@gmail.com>
2020-12-21 10:40:18 +0000

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,4 @@
-$(window).load(function() {
+$(window).on('load', function() {
   document.getElementsByClassName('sidebarDropdown').offsetHeight
   document.getElementsByClassName('sidebarDropdown').classList.remove("transFix");
 });
